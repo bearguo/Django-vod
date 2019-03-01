594d454872e57b7c8688b37b411d19e11deb4b03
bearguo <783078153@qq.com>
2019-03-01 15:26:05 +0800

    [bugfix]

--- cron.py
+++ cron.py
@@ -13,6 +13,8 @@ from epg.utils import download_m3u8_files
 from vodmanagement.models import VideoCategory, Vod
 from vodmanagement.utils import delete_vod
 
+category_id_file = config_file = Path(settings.BASE_DIR) / 'conf' / 'category_id.txt'
+
 
 def get_program():
     cf = configparser.ConfigParser()
@@ -53,8 +55,8 @@ def record_video(url, program_title):
         try:
             m3u8_file_path = parse.urlparse(url[i]).path  # /CCTV1/20180124/123456.m3u8
             video_path = settings.RECORD_MEDIA_FOLDER + m3u8_file_path
-            category_id = get_category_id()
-            print(video_path)
+            with open(category_id_file,'r', encoding='utf-8') as f:
+                category_id = f.read()
             new_record = Vod(
                     title = str(datetime.date.today()) + program_title[i],
                     video = video_path,
@@ -62,9 +64,6 @@ def record_video(url, program_title):
                     image =  str(Path(video_path).parents[0] / 'xinwenlianbo.jpg'),
                     )
             new_record.save()
-            print(str(new_record.title))
-            #p = threading.Thread(target=download_m3u8_files, args=(new_record.id, url[i], settings.RECORD_MEDIA_ROOT,))
-            #p.start()
             download_m3u8_files(new_record.id, url[i], settings.RECORD_MEDIA_ROOT)
             source = settings.STATIC_ROOT +'/xinwenlianbo.jpg'
             target = str(Path(settings.RECORD_MEDIA_ROOT + m3u8_file_path).parent) + '/xinwenlianbo.jpg'
@@ -75,13 +74,16 @@ def record_video(url, program_title):
             raise e
 
 def get_category_id():
-    try:
-        obj = VideoCategory.objects.get(name='自动录制')
-    except Exception:
-        new_category = VideoCategory(name = '自动录制',)
-        new_category.save()
-        return int(new_category.id)
-    return int(obj.id)
+    with open(category_id_file,'w', encoding='utf-8') as f:
+        try:
+            obj = VideoCategory.objects.get(name='自动录制')
+        except Exception:
+            new_category = VideoCategory(name = '自动录制',)
+            new_category.save()
+            f.write(new_category.id)
+        else:
+            f.write(obj.id)
+    
 
 def auto_record(): 
     title, channel_id = get_program()
@@ -89,7 +91,8 @@ def auto_record():
     record_video(url, program_title)
 
 def auto_del():
-    auto_record_id = get_category_id()
+    with open(category_id_file,'r', encoding='utf-8') as f:
+        auto_record_id = f.read
     now = datetime.datetime.now()
     delta = datetime.timedelta(days=7)
     obj = Vod.objects.filter(category_id=auto_record_id, timestamp__lt=(now - delta))
